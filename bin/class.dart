3bd047799101797786f4c446a68e6bce3ffb2ebd
zhofran <zhofranwijayanto@gmail.com>
2025-09-17 11:39:25 +0700

    learn method

--- bin/class.dart
+++ bin/class.dart
@@ -4,6 +4,18 @@ class Person {
   String? address;
   final String country = "Indonesia";
 
+  void sayHello(String paramName) {
+    print("Hello $paramName, my name is $name");
+  }
+
+  void Hello(){
+    print("Hello, my name is $name");
+  }
+
+  String getName() {
+    return "Hello, my name is $name";
+  }
+
 }
 
 void main() {
@@ -15,6 +27,8 @@ void main() {
   print(person1.name);
   print(person1.address);
   print(person1.country);
+
+  person1.sayHello("Joko");
 
   var person2 = Person();
   print(person2);
