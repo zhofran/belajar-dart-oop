7a0b98ce4da40196b176252bee97aa336f895299
zhofran <zhofranwijayanto@gmail.com>
2025-09-18 05:20:27 +0700

    learn constructor

---
+++ bin/constructor.dart
@@ -0,0 +1,20 @@
+class Person {
+  
+  String name = "Guest";
+  String? address;
+  final String country = "Indonesia";
+
+  Person(String paramName, String paramAddress) {
+    name = paramName;
+    address = paramAddress;
+  }
+
+}
+
+void main() {
+
+  var person = Person("Zhofran", "Depok");
+  print(person.name);
+  print(person.address);
+
+}
