06f4ee86a2d0b766b42b43b55814f7a4241ff665
zhofran <zhofranwijayanto@gmail.com>
2025-09-17 11:30:32 +0700

    learn field

--- bin/class.dart
+++ bin/class.dart
@@ -1,13 +1,22 @@
 class Person {
 
+  String name = "Guest";
+  String? address;
+  final String country = "Indonesia";
+
 }
 
 void main() {
 
   var person1 = Person();
-  print(person1);
+  person1.name = "Zhofran Dhiqo";
+  person1.address = "Jakarta";
+
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
 
   var person2 = Person();
   print(person2);
-  
+
 }
