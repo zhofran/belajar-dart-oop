1d14358bfe52cdddc096778aaf7ac3999b42d71b
zhofran <zhofranwijayanto@gmail.com>
2025-09-18 18:56:53 +0700

    learn initializer list

---
+++ bin/initializer_list.dart
@@ -0,0 +1,22 @@
+class Customer {
+  String firstName = '';
+  String lastName = '';
+  String fullName = '';
+
+  Customer (this.fullName) 
+  : firstName = fullName.split(' ')[0], 
+    lastName = fullName.split(' ')[1] {
+    
+    print('Create new customer');
+    
+  }
+}
+
+void main() {
+
+  var customer = Customer('Zhofran Dhiqo');
+  print(customer.fullName);
+  print(customer.firstName);
+  print(customer.lastName);
+
+}
